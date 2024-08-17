1ad283f6c7e2fbcefedf2c3ff0e4b799567bcbf2
Destiny <dejenkins@justworks.com>
2024-08-17 13:05:57 -0400

    controllers code added, api files and code added, models files and code added, public file added, seeds file and data added, utils files added, views files and code added

--- models/index.js
+++ models/index.js
@@ -1,1 +1,14 @@
 // associations between tables (foreign keys connected to user_id, etc)
+const User = require('./user');
+const Blog = require('./blog');
+
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+module.exports = { User, Blog };
